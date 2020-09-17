a01e2e5b1f43796eccd274a25108a9326d4519fc
Jacob Cowan <mrjcowman@gmail.com>
2020-09-17 16:47:38 -0700

    Implemented date display at top

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = $(".container");
-const today = moment().format('MMMM Do YYYY');
+let today = moment().format('MMMM Do, YYYY');
 
 const checkMarkSVG = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/></svg>';
 
@@ -25,9 +25,12 @@ const populateHoursArray = ()=>{
     console.log("Hours Array initialized");
 }    
 
-// Render the textfields in the list
+// Render the textfields in the list and the date at the top
 const renderHoursList = ()=>{
     console.log("Rendering!");
+
+    today = moment().format('MMMM Do, YYYY');
+    $("#currentDay").text(today);
     let thisHour = moment().hour();
     forEachHourInDay(hour=>{
         let thisField = $("."+formatHour(hour)+" textArea");
